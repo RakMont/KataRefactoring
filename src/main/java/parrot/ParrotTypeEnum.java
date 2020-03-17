733b061b3323059e6bc9f8cca44a4ebb9c9d7480
unknown <RakMont@gmail.com>
2020-03-17 08:19:41 -0400

    first commit

---
+++ src/main/java/parrot/ParrotTypeEnum.java
@@ -0,0 +1,8 @@
+package main.java.parrot;
+
+public enum ParrotTypeEnum {
+
+    EUROPEAN,
+    AFRICAN,
+    NORWEGIAN_BLUE;
+}
---
+++ src/test/java/parrot/ParrotTest.java
@@ -0,0 +1,58 @@
+package test.java.parrot;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import main.java.parrot.Parrot;
+import main.java.parrot.ParrotTypeEnum;
+
+public class ParrotTest {
+
+    @Test
+    public void getSpeedOfEuropeanParrot() {
+        Parrot parrot = new Parrot(ParrotTypeEnum.EUROPEAN, 0, 0, false);
+        assertEquals(parrot.getSpeed(), 12.0);
+    }
+
+    @Test
+    public void getSpeedOfAfricanParrot_With_One_Coconut() {
+        Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 1, 0, false);
+        assertEquals(parrot.getSpeed(), 3.0);
+    }
+
+    @Test
+    public void getSpeedOfAfricanParrot_With_Two_Coconuts() {
+        Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 2, 0, false);
+        assertEquals(parrot.getSpeed(), 0.0);
+    }
+
+    @Test
+    public void getSpeedOfAfricanParrot_With_No_Coconuts() {
+        Parrot parrot = new Parrot(ParrotTypeEnum.AFRICAN, 0, 0, false);
+        assertEquals(parrot.getSpeed(), 12.0);
+    }
+
+    @Test
+    public void getSpeedNorwegianBlueParrot_nailed() {
+        Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 0, true);
+        assertEquals(parrot.getSpeed(), 0.0);
+    }
+
+    @Test
+    public void getSpeedNorwegianBlueParrot_not_nailed() {
+        Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 1.5, false);
+        assertEquals(parrot.getSpeed(), 18.0);
+    }
+
+    @Test
+    public void getSpeedNorwegianBlueParrot_not_nailed_high_voltage() {
+        Parrot parrot = new Parrot(ParrotTypeEnum.NORWEGIAN_BLUE, 0, 4, false);
+        assertEquals(parrot.getSpeed(), 24.0);
+    }
+}
